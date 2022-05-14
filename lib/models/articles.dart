b64e4e89b6b66b51db6480d1283f8cee8f48958a
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-05-14 15:43:29 +0530

    added news model

---
+++ lib/models/articles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:convert' show utf8;
+
+class Article {
+  final String? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final DateTime? publishedAt;
+  final String? content;
+  Article(this.source, this.author, this.title, this.description, this.url,
+      this.urlToImage, this.publishedAt, this.content);
+  static Article fromjson(Map a) {
+    return Article(
+        (a['source'] as Map)['name'],
+        a['author'],
+        utf8.decode((a['title'] as String).runes.toList()),
+        a['description'],
+        a['url'],
+        a['urlToImage'],
+        DateTime.tryParse(a['publishedAt']),
+        a['content']);
+  }
+}
+
+class NewsCategory {
+  final String category;
+  final List<Article> stories;
+  NewsCategory(this.category, this.stories);
+  static NewsCategory fromjson(String Category, List articles) {
+    return NewsCategory(
+        Category, articles.map((a) => Article.fromjson(a)).toList());
+  }
+}
+
+class News {
+  final List<NewsCategory> AllNews;
+  News(this.AllNews);
+}
