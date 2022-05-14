3831c3af0e7fdc3cd3e1b2a6abd6d38e87ab9b06
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-05-14 15:45:06 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/provider/news_provider.dart';
+import 'package:newsapp/screens/home.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => NP(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        title: 'Material App',
+        home: Home(),
+      ),
+    );
+  }
+}
