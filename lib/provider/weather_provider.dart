1e7b58e33d36b2b0f9c2c25a66d353cd65197d84
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-05-15 17:02:49 +0530

    created a weather provider class

---
+++ lib/provider/weather_provider.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:newsapp/models/weather.dart';
+
+class WP with ChangeNotifier {
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  bool enabled = false;
+
+  final String _base = "https://api.openweathermap.org/data/2.5/onecall?";
+  final String _apiKey = '&appid=bb761635ddb3320ffc1f2148274d3436';
+  WP() {
+    init();
+  }
+
+  init() async {
+    enabled = await Geolocator.isLocationServiceEnabled();
+    notifyListeners();
+    if (enabled) {
+      Position pos = await _determinePosition();
+      var res = await http.get(
+          Uri.parse("$_base&lat=${pos.latitude}&lon=${pos.longitude}$_apiKey"));
+      weather.fromJson(jsonDecode(res.body) as Map);
+
+      //show old one and fetch new and show that
+    }
+  }
+}
