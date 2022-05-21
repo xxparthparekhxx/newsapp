4f31e1318012957502708ea6af32f53a5a01c61f
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-05-21 11:22:06 +0530

    updated weather provider

--- lib/provider/weather_provider.dart
+++ lib/provider/weather_provider.dart
@@ -6,30 +6,36 @@ import 'package:newsapp/models/loacation.dart';
 import 'package:newsapp/models/weather.dart';
 import 'package:newsapp/provider/Secret/api_key_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dart_ipify/dart_ipify.dart';
 
 class WP with ChangeNotifier {
   Future<Position> _determinePosition() async {
-    bool serviceEnabled;
     LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
 
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+
+      if (permission == LocationPermission.deniedForever ||
+          permission == LocationPermission.denied ||
+          permission == LocationPermission.unableToDetermine) {
+        final ipv4 = await Ipify.ipv4();
+        final somegeo = await Ipify.geo(KeyHandler.IPGeoKey, ip: ipv4);
+
+        return Position(
+            latitude: somegeo.location?.lat?.toDouble() ?? 0,
+            longitude: somegeo.location?.lng?.toDouble() ?? 0,
+            timestamp: DateTime.now(),
+            accuracy: 0.0,
+            speed: 69,
+            heading: 0.0,
+            altitude: 0.0,
+            speedAccuracy: 0.0,
+            floor: 0,
+            isMocked: false);
       }
     }
 
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
     return await Geolocator.getCurrentPosition();
   }
 
@@ -39,25 +45,33 @@ class WP with ChangeNotifier {
   weather? currentweather;
   Location? locationDetails;
   bool isLoading = false;
+  List<weather> todaysWeather = [];
+
+  double get maxTemp {
+    double max = -99999999;
+    for (var ele in todaysWeather) {
+      if (ele.temp > max) {
+        max = ele.temp;
+      }
+    }
+    return max;
+  }
+
+  double get minTemp {
+    double min = 99999999;
+    for (var ele in todaysWeather) {
+      if (ele.temp < min) {
+        min = ele.temp;
+      }
+    }
+    return min;
+  }
 
   WP() {
     init();
   }
 
-  init() async {
-    bool enabled = false;
-
-    LocationPermission value = await Geolocator.checkPermission();
-    if (value == LocationPermission.always ||
-        value == LocationPermission.whileInUse) {
-      enabled = true;
-    }
-    notifyListeners();
-
-    if (enabled) {
-      await getWeatherAndLocation();
-    }
-  }
+  init() async => await getWeatherAndLocation();
 
   Future getWeatherAndLocation() async {
     var p = await SharedPreferences.getInstance();
@@ -73,26 +87,33 @@ class WP with ChangeNotifier {
     }
     notifyListeners();
     Position pos = await _determinePosition();
-
-    var weatherRes = http.get(
+    //starting the weather fetching process
+    var weatherReq = http.get(
         Uri.parse("$_base&lat=${pos.latitude}&lon=${pos.longitude}$_apiKey"));
 
-    var locationRes = http.get(Uri.parse(
+    //getting the location details from the api
+    var locationReq = http.get(Uri.parse(
         "http://api.positionstack.com/v1/reverse?access_key=${KeyHandler.loactionKey}&query=${pos.latitude},${pos.longitude}"));
 
-    String weatherData = (await weatherRes).body;
-    http.Response Locationresponse = await locationRes;
+    // waiting for both requests to complete
+    String weatherData = (await weatherReq).body;
+    http.Response Locationresponse = await locationReq;
 
+    // caching the data in shared preferences
     p.setString("weather", weatherData);
     p.setString("loaction", Locationresponse.body);
     var d = (jsonDecode(weatherData) as Map);
-    print(d);
-    //getting weather
+
+    //getting Todays weather
+    (d['hourly'] as List)
+        .map((e) => todaysWeather.add(weather.fromJson(e)))
+        .toList();
+
+    //getting current weather
     currentweather = weather.fromJson(d['current']);
 
     // getting the location details eg: city,state,flag....
     locationDetails = Location.fromRes(Locationresponse.body);
-
     notifyListeners();
   }
 }
